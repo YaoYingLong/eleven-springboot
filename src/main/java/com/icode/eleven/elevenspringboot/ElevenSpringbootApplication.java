a073cf860d39cfea3d37bef2c70f70fd4ccdb3b5
yao_yinglong <yao_yinglong@icekredit.com>
2021-10-15 18:16:28 +0800

    project init

---
+++ src/main/java/com/icode/eleven/elevenspringboot/ElevenSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.icode.eleven.elevenspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevenSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElevenSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icode/eleven/elevenspringboot/ElevenSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icode.eleven.elevenspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevenSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
